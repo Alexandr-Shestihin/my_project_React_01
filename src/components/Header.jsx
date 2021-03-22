c74fb972654995b93c972f1809f5065858a42081
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-03-22 11:14:52 +0300

    22.03.21 11:14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
    return (
       <header className="header">
          <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v294-bb-41_2.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=2bc156f682aa70a342ce9e707c613927" />
-         pp
+         я тут был
       </header>
    )
 }
