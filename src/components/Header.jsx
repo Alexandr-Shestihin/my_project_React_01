6966919a33688ebb150ec4803fbf47c5bce5d9b6
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-03-22 10:52:35 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+   return (
+      <header className="header">
+         <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v294-bb-41_2.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=2bc156f682aa70a342ce9e707c613927" />
+      </header>
+   )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+const Profile = () => {
+   return (
+      <div className="content">
+         <div>
+            <img src="https://trikky.ru/wp-content/blogs.dir/1/files/2020/02/13/fioletovyj-pejzazh.jpg" />
+         </div>
+         <div>
+            ava+description
+               <img src="http://risovach.ru/upload/2019/04/generator/chernyy-vlastelin_205750111_orig_.jpg" />
+         </div>
+         <div>
+            My posts
+               <div>
+               New post
+               </div>
+            <div>
+               <div>
+                  post 1
+                  </div>
+               <div>
+                  post 2
+                  </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+export default Profile;
