cdd3cbc7af2da5d5c7eaf176d09b504375705eff
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-03-22 20:58:23 +0300

    22.03.21 21:00 Внедрение SCSS в React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Profile from './components/Profile';
-
+import For_zorax_css from './components/For_zorax_css';
 
 const App = () => {
    return (
       <div className="app-wrapper">
-         <Header />
+         <For_zorax_css />
+         {/* <Header />
          <Nav />
-         <Profile />
+         <Profile /> */}
       </div >
    );
 }
---
+++ src/components/For_zorax_css.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from "./For_zorax_css.module.scss";
+
+const For_zorax_css = () => {
+   return (
+      <div className={s.i}>
+         Я проект CSS
+      </div>
+   )
+}
+export default For_zorax_css;
