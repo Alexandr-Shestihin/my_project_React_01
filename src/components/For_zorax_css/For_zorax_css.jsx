206e47eec18d4ee24c631a31637faae515ad9dcd
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-03-27 22:30:07 +0300

    27.03.2021 22:30 Learn JS Объекты

--- src/components/For_zorax_css/For_zorax_css.jsx
+++ src/components/For_zorax_css/For_zorax_css.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import Learn from "./Learn/Learn";
 import s from "./For_zorax_css.module.scss";
-import Js from './JS/Js';
 
-const user = {
-   name: "John",
+let menu = {
+   width: 200,
+   height: 300,
+   title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+   for (let key in obj) {
+      if (typeof obj[key] == "number") {
+         obj[key] *= 2;
+      }
+   }
 }
-user.name = "Pete";
 
+multiplyNumeric(menu);
 const For_zorax_css = () => {
    return (
       <div className={s.main}>
-         <Js name={user.name} />
-         {/* <h1>Заголовок h1</h1>
-         <h2 className="list">Заголовок h2</h2>
-         <ul className="list">
-            <h4>Заголовок h4</h4>
-            <li>list 1</li>
-            <li>list 2</li>
-            <li></li>
-            <h4>Заголовок h4</h4>
-         </ul> */}
+         <Learn value={menu.width} />
       </div>
    )
 }
