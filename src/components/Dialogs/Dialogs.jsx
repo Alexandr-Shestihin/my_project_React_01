bfea8261ebe6dc7b1408012b448b8bb4d114ee6f
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-03-28 20:36:10 +0300

    28.03.2021 21 Lesson Вёрстка страницы диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,31 @@ import s from "./Dialogs.module.scss";
 
 const Dialogs = () => {
    return (
-      <div>
-         Dialogs
+      <div className={s.dialogs}>
+         <div>
+            <div className={s.dialogs_items}>
+               <div className={s.dialog + " " + s.active}>
+                  Чёрный властелин
+               </div>
+               <div className={s.dialog}>
+                  Ян
+               </div>
+               <div className={s.dialog}>
+                  Александр
+               </div>
+               <div className={s.dialog}>
+                  Даун
+               </div>
+               <div className={s.dialog}>
+                  Шкальник
+               </div>
+            </div>
+         </div>
+         <div className={s.messages}>
+            <div className="message">Hi Gitler!</div>
+            <div className="message">Sieg Hill!</div>
+            <div className="message">SS!</div>
+         </div>
       </div>
    )
 }
--- src/components/For_zorax_css/For_zorax_css.jsx
+++ src/components/For_zorax_css/For_zorax_css.jsx
@@ -2,25 +2,15 @@ import React from 'react';
 import Learn from "./Learn/Learn";
 import s from "./For_zorax_css.module.scss";
 
-let menu = {
-   width: 200,
-   height: 300,
-   title: "My menu"
-};
-
-function multiplyNumeric(obj) {
-   for (let key in obj) {
-      if (typeof obj[key] == "number") {
-         obj[key] *= 2;
-      }
-   }
-}
-
-multiplyNumeric(menu);
 const For_zorax_css = () => {
    return (
       <div className={s.main}>
-         <Learn value={menu.width} />
+         <div className={s.main_container}>
+            <div className={s.container}>
+               Text
+         </div>
+         </div>
+         {/* <Learn value={1234} /> */}
       </div>
    )
 }
