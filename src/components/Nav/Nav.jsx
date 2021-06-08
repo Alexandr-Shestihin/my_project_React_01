65058d81e3d062ebfe02fc20d0647f047ab29ed9
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2021-06-08 22:25:53 +0300

    08.06.21 Lesson 55 добавил метод componentDidMount

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,6 +13,9 @@ const Nav = (props) => {
          <div className={s.item}>
             <NavLink to="/Dialogs" activeClassName={s.active}>Massages</NavLink>
          </div>
+         <div className={s.item}>
+            <NavLink to='/Users' activeClassName={s.active} >Users</NavLink>
+         </div>
          <div className={`${s.item}`}>
             <NavLink to="/News" activeClassName={s.active}>News</NavLink>
          </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,48 +3,48 @@ import React from 'react';
 import UserPhoto from './../../files/img/userPhoto.jpg';
 import s from './Users.module.scss';
 
-const Users = (props) => {
-   let getUsers = () => {
-      if (props.users.length === 0) {
-         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-               props.setUsers(response.data.items)
-            });
-      };
+class Users extends React.Component {
+   componentDidMount() {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+         .then(response => {
+            this.props.setUsers(response.data.items)
+         });
    }
-   return (
-      <div>
-         <button onClick={getUsers} >Загрузить Пользорвателей</button>
-         {
-            props.users.map(u => <div key={u.id}>
-               <span>
-                  <div>
-                     <img src={u.photos.small ? u.photos.small : UserPhoto} className={s.usersPhoto} />
-                  </div>
-                  <div>
-                     {u.followed
-                        ? <button onClick={() => { props.unfollow(u.id) }}
-                           className={`${s.buttonUnfallow} ${s.button}`}
-                        >Unfallow</button>
-                        : <button onClick={() => { props.follow(u.id) }}
-                           className={`${s.buttonFallow} ${s.button}`}
-                        >Fallow</button>}
-                  </div>
-               </span>
-               <span>
+   render() {
+      return (
+         <div>
+            {
+               this.props.users.map(u => <div key={u.id}>
                   <span>
-                     <div>{u.name}</div>
-                     <div>{u.status}</div>
+                     <div>
+                        <img src={u.photos.small ? u.photos.small : UserPhoto} className={s.usersPhoto} />
+                     </div>
+                     <div>
+                        {u.followed
+                           ? <button onClick={() => { this.props.unfollow(u.id) }}
+                              className={`${s.buttonUnfallow} ${s.button}`}
+                           >Unfallow</button>
+                           : <button onClick={() => { this.props.follow(u.id) }}
+                              className={`${s.buttonFallow} ${s.button}`}
+                           >Fallow</button>}
+                     </div>
                   </span>
                   <span>
-                     <div>{/* u.location.country */}</div>
-                     <div>{/* u.location.city */}</div>
+                     <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                     </span>
+                     <span>
+                        <div>{/* u.location.country */}</div>
+                        <div>{/* u.location.city */}</div>
+                     </span>
                   </span>
-               </span>
-               <br /><br />
-            </div>)
-         }
-      </div >
-   )
+                  <br /><br />
+               </div>)
+            }
+         </div >
+      )
+   }
 }
+
 export default Users;
